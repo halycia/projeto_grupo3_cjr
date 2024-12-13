eea7806995144cafdca8204997a1d1de7b4a9f41
halycia <139800103+halycia@users.noreply.github.com>
2024-12-13 11:52:09 -0300

    fix: adicionar Cors para comunicação entre frontend e backend

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4000',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
+
 bootstrap();
